e35db556d1e43e8976ccc4da9011cf26e0d123d5
afolisos <70781128+afolisos@users.noreply.github.com>
2020-12-04 11:27:22 -0500

    up

--- Lab_4/scriptexam.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("btnGetMesssage").addEventListener("mouseover", message);
-
-function message(){
-  
-    document.getElementById("paragraphId2").innerHTML = "Your name here" ;
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-# afolisos.github.io
